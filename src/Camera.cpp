0dcc12da7fd0905fdbe3532c55a1538b92d98ca7
Tyler Schicke <tschicke@optonline.net>
2014-04-21 15:01:26 -0400

    added Camera and GridDetector class

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -7,21 +7,77 @@
 
 #include "Camera.h"
 
-Calibrator::Calibrator(std::string path, float sideLength, int width, int height) {
-	this->path = path;
-	this->sideLength = sideLength;
-	this->width = width;
-	this->height = height;
+Camera::Camera() {
+	camNumber = 0;
 }
 
-void Calibrator::calibrate() {
+Camera::Camera(int camNumber) {
+	this->camNumber = camNumber;
+	openStream(camNumber);
 }
 
-cv::Mat Calibrator::getCameraMatrix() {
+void Camera::openStream(int camNumber) {
+	capture.open(camNumber);
 }
 
-cv::Mat Calibrator::getDisortionCoefficients() {
+cv::Mat Camera::getCameraFrame() {
+	cv::Mat frame;
+	capture >> frame;
+	return frame;
 }
 
-void Calibrator::calcImagePoints() {
+GridDetector::GridDetector() {
+	gridWidth = gridHeight = 0;
+	squareWidth = squareHeight = 0;
+	gridDepth = 0;
 }
+
+GridDetector::GridDetector(int gridWidth, int gridHeight, int squareWidth, int squareHeight, int cameraID) {
+	init(gridWidth, gridHeight, squareWidth, squareHeight, cameraID);
+}
+
+void GridDetector::init(int gridWidth, int gridHeight, int squareWidth, int squareHeight, int cameraID) {
+	this->gridWidth = gridWidth;
+	this->gridHeight = gridHeight;
+	this->squareWidth = squareWidth;
+	this->squareHeight = squareHeight;
+
+	camera.openStream(cameraID);
+
+	gridDepth = 0;
+}
+
+void GridDetector::update(){
+	calcGridCorners2d();
+	calcGridCorners3d();
+}
+
+void GridDetector::calcGridCorners2d(){
+
+}
+
+cv::Point2f* const GridDetector::getGridCorners2d() {
+	return gridCorners2d;
+}
+
+cv::Point3f* const GridDetector::getGridCorners3d() {
+	return gridCorners3d;
+}
+//Calibrator::Calibrator(std::string path, float sideLength, int width, int height) {
+//	this->path = path;
+//	this->sideLength = sideLength;
+//	this->width = width;
+//	this->height = height;
+//}
+//
+//void Calibrator::calibrate() {
+//}
+//
+//cv::Mat Calibrator::getCameraMatrix() {
+//}
+//
+//cv::Mat Calibrator::getDisortionCoefficients() {
+//}
+//
+//void Calibrator::calcImagePoints() {
+//}
--- src/Camera.h
+++ src/Camera.h
@@ -11,6 +11,7 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/core/core.hpp>
 
 #include <string.h>
 #include <vector.h>
@@ -24,8 +25,8 @@ private:
 	float sideLength;
 	int width, height;
 
-	std::vector<std::vector<cv::Point2f>> imagePoints;
-	std::vector<std::vector<cv::Point3f>> objectPoints;
+	std::vector<std::vector<cv::Point2f> > imagePoints;
+	std::vector<std::vector<cv::Point3f> > objectPoints;
 
 public:
 	Calibrator(std::string path, float sideLength, int width, int height);
@@ -34,4 +35,47 @@ public:
 	cv::Mat getDisortionCoefficients();
 	void calcImagePoints();
 };
+
+
+class Camera {
+public:
+	Camera();
+	Camera(int camNumber);
+
+	void openStream(int camNumber);
+
+	cv::Mat getCameraFrame();
+
+private:
+	cv::VideoCapture capture;
+
+	int camNumber;
+};
+
+class GridDetector {
+public:
+	GridDetector();
+	GridDetector(int gridWidth, int gridHeight, int squareWidth, int squareHeight, int cameraID);
+
+	void init(int gridWidth, int gridHeight, int squareWidth, int squareHeight, int cameraID);
+
+	void update();
+
+	cv::Point2f *const getGridCorners2d();
+	cv::Point3f *const getGridCorners3d();
+
+	void calcGridCorners2d();
+	void calcGridCorners3d();
+private:
+
+	int gridWidth, gridHeight;//width and height in number of squares
+	int squareWidth, squareHeight;//width and height of one square in centimeters
+
+	Camera camera;
+
+	cv::Point2f gridCorners2d[4];
+	cv::Point3f gridCorners3d[4];
+	float gridDepth;
+};
+
 #endif /* CAMERA_H_ */
